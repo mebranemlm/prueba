9bd9ac946d83772eebc1177833de3e0c87dd867c
mebranemlm <none>
2016-04-16 20:02:01 -0500

    Titulo prueba3

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Hola Mundo</title>
+</head>
+<body>
+	<h1>Branch: Prueba 3</h1>
+	<h1>Título</h1>
+	<h2>Titulo 2</h2>
+	<h3>Titulo 3</h3>
+
+	Hola Mundo 1
+	Hola Mundo 2
+	Hola Mundo 3
+	Hola Mundo 4
+	Hola Mundo 5
+	Hola Mundo 6
+<style type="text/css">
+
+h1{
+	text-align: center;
+	color: #BE3838;
+}
+h2{
+	color: #223C9C;
+}
+
+
+</style>
+
+<script type="text/javascript">
+	document.getElemetsByTagName('h1');
+
+</script>
+<?php
+
+?>
+</body>
+</html>
